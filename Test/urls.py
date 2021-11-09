3a32e2f8a20106ee5e85e13fb53796d81ab69494
Hammad58343 <hammadahmed.eirtechs@gmail.com>
2021-11-09 21:32:49 +0500

    first commit

---
+++ Test/urls.py
@@ -0,0 +1,29 @@
+"""Test URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.http.response import HttpResponse, JsonResponse
+from django.urls import path
+import requests
+
+def view(request):
+    return JsonResponse(data=requests.get("https://www.uuidtools.com/api/generate/v1/count/10").json(),status=200,safe=False)
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("",view)
+]
