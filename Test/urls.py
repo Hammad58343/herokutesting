d8167922aed0d89ba2ef556d4b13cb5124e57d1a
Hammad58343 <hammadahmed.eirtechs@gmail.com>
2021-11-10 19:15:02 +0500

    changes

--- Test/urls.py
+++ Test/urls.py
@@ -21,31 +21,20 @@ from django.http.response import HttpResponse, JsonResponse
 from django.urls import path
 import requests
 
-# def view(request):
-#     headers = {
-#     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36 OPR/80.0.4170.72'
-#     }
+def view(request):
+    headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36 OPR/80.0.4170.72'
+    }
 
-#     r = requests.get("https://www.uuidtools.com/api/generate/v1/count/100")
-#     if r.status_code == 200: 
-#         r.raise_for_status()
-#         data = r.json()
-
-#     else:
-#         data = {"success": "false"}
-       
-#     return JsonResponse(data = data,status=200,safe=False)
-
-def view():
-    url = 'https://www.uuidtools.com/api/generate/v1/count/100'
-    data = {'success': 'false'}
-    try:
-        r = requests.get(url, params=data)
+    r = requests.get("https://www.uuidtools.com/api/generate/v1/count/100")
+    if r.status_code == 200: 
         r.raise_for_status()
-    except requests.exceptions.HTTPError as err:
-        print(err)
-    
-    return r.json()
+        data = r.json()
+
+    else:
+        data = {"success": "false"}
+       
+    return JsonResponse(data = data,status=200,safe=False)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
