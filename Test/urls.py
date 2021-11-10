72261982133d2a76714ed52ad62f8ad9fd06f84a
Hammad58343 <hammadahmed.eirtechs@gmail.com>
2021-11-10 16:11:43 +0500

    changes

--- Test/urls.py
+++ Test/urls.py
@@ -19,25 +19,21 @@ from django.contrib import admin
 from django.http import response
 from django.http.response import HttpResponse, JsonResponse
 from django.urls import path
-from django.http import request
+import requests
 
 def view(request):
     headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36 OPR/80.0.4170.72'
     }
 
-    r = request.get("https://www.uuidtools.com/api/generate/v1/count/100")
-    r.status_code == 200
-    response.raise_for_status()  # raises exception when not a 2xx response
-    if response.status_code != 204:
-        return response.json()
-    # if r.status_code == 200: 
-    #     data = r.json()
+    r = requests.get("https://www.uuidtools.com/api/generate/v1/count/100")
+    if r.status_code == 200: 
+        data = r.json()
 
-    # else:
-    #     data = {"success": "false"}
+    else:
+        data = {"success": "false"}
        
-    # return JsonResponse(data = data,status=200,safe=False)
+    return JsonResponse(data = data,status=200,safe=False)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
